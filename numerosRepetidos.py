d52a1bcba77165f7120ef442a46980f549e9cca9
FrancoBertoldiMariglio <f.bertoldi@alumno.um.edu.ar>
2022-10-12 10:06:05 -0300

    12/10/2022

--- numerosRepetidos.py
+++ numerosRepetidos.py
@@ -1,13 +1,20 @@
 
 
-def numRep(lista):
-    diccionario = {}
-    for x in lista:
-        if type(x) == str:
-            return "La lista solo puede contener numeros"
-    for i in lista:
-        if i in diccionario:
-            diccionario[i] += 1
-        else:
-            diccionario[i] = 1
-    return diccionario
+class numRep():
+
+    def __init__(self, lista):
+        self.lista = lista
+
+    def checkValid(self):
+        for x in self.lista:
+            if type(x) == str:
+                return "La lista solo puede contener numeros"
+
+    def rep(self):
+        diccionario = {}
+        for i in self.lista:
+            if i in diccionario:
+                diccionario[i] += 1
+            else:
+                diccionario[i] = 1
+        return diccionario
--- test_numerosRepetidos.py
+++ test_numerosRepetidos.py
@@ -4,17 +4,20 @@ from numerosRepetidos import numRep
 
 class TestNum(unittest.TestCase):
 
-    def test_1(self):
-        dict = numRep([0, 1, 2, 3])
+    def test_checkValid(self):
+        clase = numRep(["a", "b", 1, 2])
+        dict = clase.checkValid()
+        self.assertEqual("La lista solo puede contener numeros", dict)
+
+    def test_rep1(self):
+        clase = numRep([0, 1, 2, 3])
+        dict = clase.rep()
         self.assertEqual({0: 1, 1: 1, 2: 1, 3: 1}, dict)
 
-    def test_2(self):
-        dict = numRep([0, 0, 1, 2, 1, 3])
+    def test_rep2(self):
+        clase = numRep([0, 0, 1, 2, 1, 3])
+        dict = clase.rep()
         self.assertEqual({0: 2, 1: 2, 2: 1, 3: 1}, dict)
-
-    def test_3(self):
-        dict = numRep(["a", "b", 1, 2])
-        self.assertEqual("La lista solo puede contener numeros", dict)
 
 
 if __name__ == '__main__':
